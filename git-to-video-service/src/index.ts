f0827de149e5fa605d56ef58e80d0644226b0494
Ashwin Madhavan <ashwinmadhavan12@gmail.com>
2023-09-23 14:58:21 -0700

    Edits to deploy service on GCP

--- git-to-video-service/src/index.ts
+++ git-to-video-service/src/index.ts
@@ -1,73 +1,63 @@
-// import express from 'express';
-// import ffmpeg from 'fluent-ffmpeg';
-
-// const app = express();
-// app.use(express.json());
-
-
-// app.post('/process-video', (req, res) => {
-
-//   // Get the path of the input video file from the request body
-//   const inputFilePath = req.body.inputFilePath;
-//   const outputFilePath = req.body.outputFilePath;
-
-//   // Check if the input file path is defined
-//   if (!inputFilePath || !outputFilePath) {
-//     return res.status(400).send('Bad Request: Missing file path');
-//   }
-
-//   // Create the ffmpeg command
-//   ffmpeg(inputFilePath)
-//     .outputOptions('-vf', 'scale=-1:360') // 360p
-//     .on('end', function() {
-//         console.log('Processing finished successfully');
-//         res.status(200).send('Processing finished successfully');
-//     })
-//     .on('error', function(err: any) {
-//         console.log('An error occurred: ' + err.message);
-//         res.status(500).send('An error occurred: ' + err.message);
-//     })
-//     .save(outputFilePath);
-// });
-
-// const port = process.env.PORT || 3000;
-// app.listen(port, () => {
-//     console.log(`Server is running on port ${port}`);
-// });
-
 import express from 'express';
 import ffmpeg from 'fluent-ffmpeg';
 import { exec } from 'child_process';
+import { 
+    uploadProcessedVideo,
+    downloadRawVideo,
+    deleteRawVideo,
+    deleteProcessedVideo,
+    convertVideo,
+    setupDirectories
+  } from './storage';
+  
+setupDirectories();
 
 const app = express();
 app.use(express.json());
 
-app.post('/process-git', (req, res) => {
-  const inputFilePath = req.body.inputFilePath;
-  const outputFilePath = req.body.outputFilePath;
-
-  if (!inputFilePath || !outputFilePath) {
-    return res.status(400).json({ error: 'File path not provided.' });
-  }
-
-  // Command to generate Gource visualization and save as .mp4
-  const gourceCommand = `xvfb-run gource -1280x720 -o - ${inputFilePath} | ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - -vcodec libx264 -preset ultrafast -pix_fmt yuv420p -crf 18 -threads 0 -bf 0 ${outputFilePath}`;
-  // potentially can run the above without xvfb(a Docker install requirement) using:
-  // const gourceCommand = `xvfb-run gource -1280x720 -o - ${inputFilePath} | ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - -pix_fmt yuv420p ${outputFilePath}`;
-
-  // Execute the command
-  exec(gourceCommand, (error, stdout, stderr) => {
-    if (error) {
-        return res.status(500).json({ error: 'Failed to generate Gource visualization!', stderr });
+app.post('/process-git', async (req, res) => {
+    // Get the bucket and filename from the Cloud Pub/Sub message
+    let data;
+    try {
+      const message = Buffer.from(req.body.message.data, 'base64').toString('utf8');
+      data = JSON.parse(message);
+      if (!data.name) {
+        throw new Error('Invalid message payload received.');
+      }
+    } catch (error) {
+      console.error(error);
+      return res.status(400).send('Bad Request: missing filename.');
     }
-
-    // Gource visualization successfully created
-    return res.status(200).json({ message: 'Gource visualization created and saved as gource.mp4.' });
+  
+    const inputFileName = data.name;
+    const outputFileName = `processed-${inputFileName}`;
+  
+    // Download the raw video from Cloud Storage
+    await downloadRawVideo(inputFileName);
+  
+    // Process the video into 360p
+    try { 
+      await convertVideo(inputFileName, outputFileName)
+    } catch (err) {
+      await Promise.all([
+        deleteRawVideo(inputFileName),
+        deleteProcessedVideo(outputFileName)
+      ]);
+      return res.status(500).send('Processing failed');
+    }
+    
+    // Upload the processed video to Cloud Storage
+    await uploadProcessedVideo(outputFileName);
+  
+    await Promise.all([
+      deleteRawVideo(inputFileName),
+      deleteProcessedVideo(outputFileName)
+    ]);
+  
+    return res.status(200).send('Processing finished successfully');
   });
-});
 
-const PORT = 3000; // Set the desired port number
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+  });
---
+++ git-to-video-service/src/storage.ts
@@ -0,0 +1,143 @@
+import { Storage } from "@google-cloud/storage";
+import { exec } from 'child_process';
+import fs from 'fs';
+import ffmpeg from 'fluent-ffmpeg';
+
+
+const storage = new Storage(); //instance of cloud storage to interact with API
+
+const rawVideoBucketName = "am19-raw-git-files";
+const processedVideoBucketName = "am19-gource-videos";
+
+const localRawVideoPath = "./raw-git-files";
+const localProcessedVideoPath = "./gource-videos";
+
+/**
+ * Creates the local directories for raw and processed videos.
+ * Ensures that these directories exist in our container.
+ */
+export function setupDirectories() {
+  ensureDirectoryExistence(localRawVideoPath);
+  ensureDirectoryExistence(localProcessedVideoPath);
+}
+
+
+/**
+ * @param rawVideoName - The name of the file to convert from {@link localRawVideoPath}.
+ * @param processedVideoName - The name of the file to convert to {@link localProcessedVideoPath}.
+ * @returns A promise that resolves when the video has been converted.
+ */
+export function convertVideo(rawVideoName: string, processedVideoName: string) {
+  return new Promise<void>((resolve, reject) => {
+      const gourceCommand = `xvfb-run gource -1280x720 -o - ${localRawVideoPath}/${rawVideoName} | ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - -vcodec libx264 -preset ultrafast -pix_fmt yuv420p -crf 18 -threads 0 -bf 0 ${localProcessedVideoPath}/${processedVideoName}`;
+      
+      exec(gourceCommand, (error, stdout, stderr) => {
+        if (error) {
+            console.log('Failed to generate Gource visualization! Error:', stderr);
+            reject(error)
+        }
+    
+        // Gource visualization successfully created
+        console.log('Gource visualization created and saved as gource.mp4.');
+        resolve();
+      });
+  });
+}
+
+
+/**
+ * @param fileName - The name of the file to download from the 
+ * {@link rawVideoBucketName} bucket into the {@link localRawVideoPath} folder.
+ * @returns A promise that resolves when the file has been downloaded.
+ */
+export async function downloadRawVideo(fileName: string) {
+  await storage.bucket(rawVideoBucketName)
+    .file(fileName)
+    .download({
+      destination: `${localRawVideoPath}/${fileName}`,
+    });
+
+  console.log(
+    `gs://${rawVideoBucketName}/${fileName} downloaded to ${localRawVideoPath}/${fileName}.`
+  );
+}
+
+
+/**
+ * @param fileName - The name of the file to upload from the 
+ * {@link localProcessedVideoPath} folder into the {@link processedVideoBucketName}.
+ * @returns A promise that resolves when the file has been uploaded.
+ */
+export async function uploadProcessedVideo(fileName: string) {
+  const bucket = storage.bucket(processedVideoBucketName);
+
+  // Upload video to the bucket
+  await storage.bucket(processedVideoBucketName)
+    .upload(`${localProcessedVideoPath}/${fileName}`, {
+      destination: fileName,
+    });
+  console.log(
+    `${localProcessedVideoPath}/${fileName} uploaded to gs://${processedVideoBucketName}/${fileName}.`
+  );
+
+  // Set the video to be publicly readable
+  await bucket.file(fileName).makePublic();
+}
+
+
+/**
+ * @param fileName - The name of the file to delete from the
+ * {@link localRawVideoPath} folder.
+ * @returns A promise that resolves when the file has been deleted.
+ * 
+ */
+export function deleteRawVideo(fileName: string) {
+  return deleteFile(`${localRawVideoPath}/${fileName}`);
+}
+
+
+/**
+* @param fileName - The name of the file to delete from the
+* {@link localProcessedVideoPath} folder.
+* @returns A promise that resolves when the file has been deleted.
+* 
+*/
+export function deleteProcessedVideo(fileName: string) {
+  return deleteFile(`${localProcessedVideoPath}/${fileName}`);
+}
+
+
+/**
+ * @param filePath - The path of the file to delete.
+ * @returns A promise that resolves when the file has been deleted.
+ */
+function deleteFile(filePath: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    if (fs.existsSync(filePath)) {
+      fs.unlink(filePath, (err) => {
+        if (err) {
+          console.error(`Failed to delete file at ${filePath}`, err);
+          reject(err);
+        } else {
+          console.log(`File deleted at ${filePath}`);
+          resolve();
+        }
+      });
+    } else {
+      console.log(`File not found at ${filePath}, skipping delete.`);
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * Ensures a directory exists, creating it if necessary.
+ * @param {string} dirPath - The directory path to check.
+ */
+function ensureDirectoryExistence(dirPath: string) {
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath, { recursive: true }); // recursive: true enables creating nested directories
+    console.log(`Directory created at ${dirPath}`);
+  }
+}
